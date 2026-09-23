f45c05f28e386224bdb5e0ab63e8ac4cbdcaa91a
ALNOOR LOYALTY <alnoorcafe1995@gmail.com>
2026-09-23 22:05:01 +0400

    Update Supabase URL in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 
 window.AL_NOOR_CONFIG = {
-  SUPABASE_URL: "https://fgarlyoqobopudfkifbp.supabase.co",
+  SUPABASE_URL: "https://hlzmnbmngsbvnlnaaoau.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_hmDY0Nch_M_WPIMRXW26-HA_eaLaVZcY"
 };
